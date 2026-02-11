4399547b726f400e495b5a444025ba754ae4628e
mohammad reza sarkhosh <178334659+MohamadRezaSarkhosh@users.noreply.github.com>
2026-02-11 21:16:23 +0330

    Create Profile Class

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from accounts.models import User
+from accounts.models import User, Profile
 
 
 class CustomUserAdmin(UserAdmin):
@@ -40,3 +40,4 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
