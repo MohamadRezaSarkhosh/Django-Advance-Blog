16650544985f143560f6a08f6f0a421ab507f5a4
mohammad reza sarkhosh <178334659+MohamadRezaSarkhosh@users.noreply.github.com>
2026-02-11 23:27:48 +0330

    use get user model for models blog and register into admin.py

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,15 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class Post(models.Model):
     '''
     this is a class to define posts for blog app
     '''
-    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
     title = models.CharField(max_length=250)
     content = models.TextField()
     status = models.BooleanField()
-    category = models.ForeignKey('category', on_delete=models.SET_NULL)
+    category = models.ForeignKey('category', on_delete=models.SET_NULL, null=True)
     
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
