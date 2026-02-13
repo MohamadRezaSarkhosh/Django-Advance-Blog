90d0124101ff6a32c182e41f2133ef8e88cfc2b7
mohammad reza sarkhosh <178334659+MohamadRezaSarkhosh@users.noreply.github.com>
2026-02-13 16:41:33 +0330

    CBV
    redirect view

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from blog import views
 
+app_name = 'blog'
 
 
 urlpatterns = [
     path('cbv-view', views.IndexView.as_view(), name='cbv-view'),
+    path('go-to-maktabkhoone/<int:pk>', views.RedirectToMaktab.as_view(), name='redirect-to-maktabkhoone')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.base import TemplateView, RedirectView
 from blog.models import Post
 
 
 
 class IndexView(TemplateView):
+    """
+    a class base view to show index page
+    """
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
@@ -12,3 +15,15 @@ class IndexView(TemplateView):
         context['name'] = 'mamzy'
         context['posts'] = Post.objects.all()
         return context
+
+
+class RedirectToMaktab(RedirectView):
+    """
+    CBV for redirect view
+    """
+    url = "https://maktabkhooneh.org"
+
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
