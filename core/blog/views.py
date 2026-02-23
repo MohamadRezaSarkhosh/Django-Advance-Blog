366ccef584107e19ab98367f6dfa5eb783302459
mohammad reza sarkhosh <178334659+MohamadRezaSarkhosh@users.noreply.github.com>
2026-02-23 21:18:42 +0330

    CBV -- DeleteView -- Complete CRUD

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import TemplateView, RedirectView
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from blog.models import Post
 from blog.forms import PostForm
 
@@ -61,4 +61,9 @@ class PostCreateView(CreateView):
 class PostEditView(UpdateView):
     model = Post
     form_class = PostForm
+    success_url = '/blog/post/'
+
+
+class PostDeleteView(DeleteView):
+    model = Post
     success_url = '/blog/post/'
