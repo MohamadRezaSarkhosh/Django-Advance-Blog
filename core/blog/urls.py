592b0d12562c220cc2d56bb452c5bb0aa4096f8e
mohammad reza sarkhosh <178334659+MohamadRezaSarkhosh@users.noreply.github.com>
2026-02-24 22:25:13 +0330

    test api rest_framework

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -7,9 +7,10 @@ app_name = 'blog'
 urlpatterns = [
     # path('cbv-view', views.IndexView.as_view(), name='cbv-view'),
     # path('go-to-maktabkhoone/<int:pk>', views.RedirectToMaktab.as_view(), name='redirect-to-maktabkhoone'),
-    path('post/', views.PostList.as_view(), name='post-list'),
-    path('post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
-    path('post/create/', views.PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post-edit'),
-    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete')
+    # path('post/', views.PostList.as_view(), name='post-list'),
+    # path('post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
+    # path('post/create/', views.PostCreateView.as_view(), name='post-create'),
+    # path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post-edit'),
+    # path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/', views.api_post_list_view, name='post-list'),
 ]
