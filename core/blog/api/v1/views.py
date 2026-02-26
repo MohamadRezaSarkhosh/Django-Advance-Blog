f9d031f9c89ffdcbe0de0e0ead613a8dacb621da
mohammad reza sarkhosh <178334659+MohamadRezaSarkhosh@users.noreply.github.com>
2026-02-26 23:49:22 +0330

    used from model serializer

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,18 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from blog.models import Post
-from .serializers import PostSerializers
+from .serializers import PostSerializer
 from django.shortcuts import get_object_or_404
 
 @api_view()
 def postList(request):
     posts = Post.objects.filter(status=True)
-    serializer = PostSerializers(posts,many=True)
+    serializer = PostSerializer(posts,many=True)
     return Response(serializer.data)
 
 
 @api_view()
 def postDetail(request, id):
     post = get_object_or_404(Post, pk=id, status=True)
-    serializers = PostSerializers(post)
+    serializers = PostSerializer(post)
     return Response(serializers.data)
