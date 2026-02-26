6a338e931f3dcb9699746037163d41e36e7c7eb2
mohammad reza sarkhosh <178334659+MohamadRezaSarkhosh@users.noreply.github.com>
2026-02-26 23:00:07 +0330

    Handling a post not found 404

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from blog.models import Post
 from .serializers import PostSerializers
-
+from django.shortcuts import get_object_or_404
 
 @api_view()
 def postList(request):
@@ -11,6 +11,6 @@ def postList(request):
 
 @api_view()
 def postDetail(request, id):
-    post = Post.objects.get(pk=id)
+    post = get_object_or_404(Post, pk=id)
     serializers = PostSerializers(post)
     return Response(serializers.data)
