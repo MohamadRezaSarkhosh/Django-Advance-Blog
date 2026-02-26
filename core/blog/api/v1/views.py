d934238f3b6d156975c1507d6973f501fd31fe52
mohammad reza sarkhosh <178334659+MohamadRezaSarkhosh@users.noreply.github.com>
2026-02-26 19:32:11 +0330

    using a simple serializers for blog/api/v1

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,7 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from blog.models import Post
+from .serializers import PostSerializers
 
 
 @api_view()
 def postList(request):
     return Response({"name" : "MohamadReza"})
+
+
+@api_view()
+def postDetail(request, id):
+    post = Post.objects.get(pk=id)
+    serializers = PostSerializers(post)
+    return Response(serializers.data)
